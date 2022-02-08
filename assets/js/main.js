43f861bf8b1e331af8169119f17ea028599695de
SuperCoder <supercoder@AirvonSperCoder.fritz.box>
2022-02-08 15:25:38 +0100

    2nd

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,20 +2,17 @@ let element = document.getElementById("element");
 
 
 function classSmall() {
-    console.log("small")
     element.classList.remove("mid");
     element.classList.remove("big");
     element.classList.add("small");
 }
 function classMiddel() {
-    console.log("mid");
     element.classList.remove("small");
     element.classList.remove("big");
     element.classList.add("middel");
 
 }
 function classBig() {
-    console.log("big");
     element.classList.remove("small");
     element.classList.remove("mid");
     element.classList.add("big");
